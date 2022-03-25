d8f1ad7ddd6f05d1f3ac4352c4d191b75e93385d
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2022-03-25 18:50:20 +0100

    BigQuery integration

--- dags/amazing_scraper_dag.py
+++ dags/amazing_scraper_dag.py
@@ -12,7 +12,7 @@ if not shutil.which("virtualenv"):
     )
 else:
 
-    @dag(dag_id= 'amazing_scraper', schedule_interval='@daily', start_date=datetime(2021, 1, 1), catchup=False, tags=['scrapper'])
+    @dag(dag_id= 'amazing_scraper', schedule_interval='@daily', start_date=datetime(2021, 1, 1), catchup=False, tags=['scraping_project'])
     def taskflow():
         """
         ### TaskFlow API example using virtualenv
@@ -22,10 +22,10 @@ else:
 
         @task.virtualenv(
             requirements=[
-                'globalfirepower-scraper==0.0.1',
+                'globalfirepower-scraper==0.0.2',
             ],
             python_version=3.9,
-            system_site_packages=False
+            system_site_packages=True
         )
         def extract():
             """
@@ -34,37 +34,69 @@ else:
             """
             from globalfirepower import GlobalFirePowerScraper
 
-            scraper = GlobalFirePowerScraper
+            scraper = GlobalFirePowerScraper()
             
-            return scraper.get_country_ranking_table()
+            return scraper.get_armies_information().to_dict()
 
         @task.virtualenv(
             requirements=[
-                'globalfirepower-scraper==0.0.1',
+                'pandas'
             ],
             python_version=3.9,
-            system_site_packages=False
+            system_site_packages=True
         )
-        def transform(ranking):
+        def transform(ranking: dict):
             """
             #### Transform task
-            Using the Countries Ranking table, get some details about
-            each country and complete the data.
+            Extract the variables we want and make some transformations
             """
-            from globalfirepower import GlobalFirePowerScraper
+            import pandas as pd
 
-            scraper = GlobalFirePowerScraper
+            ranking_df = pd.DataFrame.from_dict(ranking)
+            ranking_df = ranking_df[[
+                'rank',
+                'name',
+                'power_index',
+                'progress',
+                'Total Population',
+                'Available Manpower',
+                'Fit-for-Service',
+                'Reaching Mil Age Annually',
+                'Tot Military Personnel (est.)',
+                'Active Personnel',
+                'Reserve Personnel',
+                'Paramilitary Total'
+            ]].copy()
 
-            return scraper.get_countries_details(ranking)
-            
+            ranking_df.rename(columns={
+                'Total Population': 'total_population',
+                'Available Manpower': 'available_manpower',
+                'Fit-for-Service': 'fit_for_service',
+                'Reaching Mil Age Annually': 'reaching_military_age_annually',
+                'Tot Military Personnel (est.)': 'total_estimated_military_personnel',
+                'Active Personnel': 'active_personnel',
+                'Reserve Personnel': 'reserve_personnel',
+                'Paramilitary Total': 'paramilitary_total'
+            }, inplace=True)
 
-        @task()
-        def load(countries_details):
+            return ranking_df.to_dict()
+
+        @task.virtualenv(
+            requirements=[
+                'pandas'
+            ],
+            python_version=3.9,
+            system_site_packages=True
+        )
+        def load(countries_details: dict):
             """
             #### Load task
-            Print the first lines of the Dataset
+            Load the dictionary into BigQuery Dataset
             """
-            print(countries_details.head())
+            import pandas as pd
+
+            countries_details_df = pd.DataFrame.from_dict(countries_details)
+            print(countries_details_df.head())
 
         countries_ranking = extract()
         countries_information = transform(countries_ranking)
